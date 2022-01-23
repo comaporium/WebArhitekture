ab67aad1ce9f694fa861d4026d06a26582902742
benjamingrbo <benjamin.grbo@fsk.unsa.ba>
2022-01-23 03:29:35 +0100

    haman haman kraj

--- projekat3/skripta.js
+++ projekat3/skripta.js
@@ -7,9 +7,26 @@ $(document).ready(function (){
     })
 }); 
 
+
 function provjeri() {
     let adresa = document.getElementById("adresa").value;
+    if (adresa == "" || adresa == null) {
+        alert("Unijeli ste prazno polje, ponovite unos.");
+        return false;
+    }
+
+    if (adresa.length > 15) {
+        document.getElementById("izlaz").innerHTML = "Unijeli ste neispravnu IP adresu.";
+        return false;
+    }
+
     let dijelovi = adresa.split(".");
+
+    if(dijelovi.length > 4 || dijelovi.length < 4){
+        document.getElementById("izlaz").innerHTML = "Unijeli ste neispravnu IP adresu.";
+        return false;
+    }
+
     let numerickiDijelovi = [];
 
     //iskoristiti cemo poslije 
@@ -17,48 +34,32 @@ function provjeri() {
     let opseg2 = "172.16.0.0 - 172.31.255.255";
     let opseg3 = "192.168.0.0 - 192.168.255.255";
 
-    //Ako adresa ima vise od 15 karaktera neispravna je
-
-    if(adresa.length == 0){
-        document.getElementById("izlaz").innerHTML = "Unijeli ste prazno polje, ponovite unos.";
-    }
-
-    if (adresa.length > 15) {
-        document.getElementById("izlaz").innerHTML = "Unijeli ste neispravnu IP adresu.";
-    }
-
-    //ako adresa ima vise od 4 okteta neispravna je
-
-    if (dijelovi.size > 4) {
-        document.getElementById("izlaz").innerHTML = "Unijeli ste neispravnu IP adresu.";
-    }
-
     //pretvaramo string u integere kako bi mogli raditi poredjenje
 
     for (let i = 0; i < dijelovi.length; i++) {
         numerickiDijelovi.push(parseInt(dijelovi[i]));
     }
+    console.log(dijelovi);
+    console.log(numerickiDijelovi);
 
     //ako je vrijednost nekog okteta veca od 255 ili manja od 0 neispravna je adresa
 
     if (((numerickiDijelovi[0] > 255) || (numerickiDijelovi[0] < 0)) || ((numerickiDijelovi[1] > 255) || (numerickiDijelovi[1] < 0))
         || ((numerickiDijelovi[2] > 255) || (numerickiDijelovi[2] < 0)) || ((numerickiDijelovi[3] > 255) || (numerickiDijelovi[3] < 0))) {
         document.getElementById("izlaz").innerHTML = "Unijeli ste neispravnu IP adresu.";
+        return false;
     }
 
+
     //ako je ispravna 
-    if (numerickiDijelovi[0] == 10) {
-        document.getElementById("ispis").innerHTML = "Unesena IP adresa je ";
-        document.getElementById("opseg").innerHTML = "privatna i pripada opsegu " + opseg1;
-    } else if ((numerickiDijelovi[0] == 172) && (16 <= numerickiDijelovi[1] <= 31)) {
-        document.getElementById("ispis").innerHTML = "Unesena IP adresa je ";
-        document.getElementById("opseg").innerHTML = "privatna i pripada opsegu " + opseg2;
-    } else if ((numerickiDijelovi[0] == 192) && (numerickiDijelovi[1] == 168)) {
-        document.getElementById("ispis").innerHTML = "Unesena IP adresa je ";
-        document.getElementById("opseg").innerHTML = "privatna i pripada opsegu " + opseg3;
+    if (numerickiDijelovi[0] === 10) {
+        document.getElementById("izlaz").innerHTML = "Unesena IP adresa je privatna i pripada opsegu " + opseg1;
+    } else if ((numerickiDijelovi[0] === 172) && (16 <= numerickiDijelovi[1] <= 31)) {
+        document.getElementById("izlaz").innerHTML = "Unesena IP adresa je privatna i pripada opsegu " + opseg2;
+    } else if ((numerickiDijelovi[0] === 192) && (numerickiDijelovi[1] === 168)) {
+        document.getElementById("izlaz").innerHTML = "Unesena IP adresa je privatna i pripada opsegu " + opseg3;
     } else {
-        document.getElementById("ispis").innerHTML = "Unesena IP adresa je ";
-        document.getElementById("opseg").innerHTML = "javna.";
+        document.getElementById("izlaz").innerHTML = "Unesena IP adresa je javna";
     }
 
 }
